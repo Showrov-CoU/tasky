47bd9e1ca9e738647c3b14895128a638357d411f
Showrov-CoU <showrov.cse.cou@gmail.com>
2024-05-28 18:47:46 +0600

    Initialize laravel crud project

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\welcomeController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function(){
+//     return view('welcome');
+// });
+
+Route::get('/', [welcomeController::class, 'welcome'])->name('welcome');
+
+// Route::get('/notes', [NoteController::class, 'index'])->name('note');
+// Route::get('/notes/create', [NoteController::class, 'create'])->name('note.create');
+// Route::post('/notes', [NoteController::class, 'store'])->name('note.store');
+// Route::get('/notes/{id}', [NoteController::class, 'show'])->name('note.show');
+// Route::get('/notes/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+// Route::put('/notes/{id}', [NoteController::class, 'update'])->name('note.update');
+// Route::delete('/notes/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+  
+Route::resource('notes', NoteController::class);
